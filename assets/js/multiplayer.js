3b6fdd5603173a49ade5f6b2102308662adf4400
nikoletta <grishaberN@outlook.com>
2023-07-25 10:34:53 +0200

    Get DOM elements and assign them to variables

--- assets/js/multiplayer.js
+++ assets/js/multiplayer.js
@@ -1,3 +1,6 @@
+/* jshint esversion:8 */
+
+// Get DOM elements and assign them to variables
 let btnQuitOpen = document.querySelector('#quit-button');
 let btnRulesOpen = document.querySelector('#button_instruction')
 let btnQuitClose = document.querySelector('#close-modal');
@@ -45,3 +48,41 @@ let closeModalRules = function () {
 /**
  * Game Logic
  */
+// Get DOM elements and assign them to variables
+const player1Buttons = document.querySelectorAll(".player-0 .hand");
+const player2Buttons = document.querySelectorAll(".player-1 .hand");
+const player1ChoiceImage = document.getElementById("player1-choice-image");
+const player2ChoiceImage = document.getElementById("player2-choice-image");
+const refereeText = document.querySelector(".referee h1");
+const playAgainButton = document.querySelector(".play-again");
+const scorePlayer1 = document.getElementById("score-player1");
+const scorePlayer2 = document.getElementById("score-player2");
+const resultsDiv = document.querySelector(".results");
+const contentDiv = document.querySelector(".content");
+const refereeDiv = document.querySelector(".referee");
+const player0Div = document.querySelector(".player-0");
+const player1Div = document.querySelector(".player-1");
+
+const choices = [
+    {
+        name: "rock",
+        imageSrc: "rock.png",
+        beats: "scissors"
+    },
+    {
+        name: "paper",
+        imageSrc: "paper.jpg",
+        beats: "rock"
+    },
+    {
+        name: "scissors",
+        imageSrc: "scissors.png",
+        beats: "paper"
+    }
+];
+
+let userScore = 0;
+let player2Score = 0;
+let player1Choice = null;
+let player2Choice = null;
+let gameResult = null;
